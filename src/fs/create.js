526e4fedafc42c36d5b2197c8b94d7174785e752
a.karko <antonkarko33@gmail.com>
2022-06-05 21:05:06 +0300

    File system task 1(create)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
-export const create = async () => {
+import { create } from 'fs';
+// import { unlink } from 'fs'
+
+export const create = async (args) => {
     // Write your code here 
+    // try {
+    //     await unlink('./files/fresh.txt');
+    //     console.log('I am fresh and young');
+    // } catch (error) {
+    //     console.error('FS operation failed', error.message);
+    // }
+
+    args('./files/fresh.txt', 'w', "I am fresh and young", (err) => {
+        if (err) throw err
+        console.log('FS operation failed');
+    });
+    //     unlink('./files/fresh.txt', (err) => {
+    //         if (err) throw err;
+    //         console.log('FS operation failed');
+    //     });
 };
+create();
